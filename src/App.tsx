a7178faba030724bc20fc52e1af46ae5298bc65f
rauljariasz <rauljariasz@gmail.com>
2024-07-16 12:27:07 -0400

    FIX some issues

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Footer from './components/organisms/Footer';
 import './App.css';
 export default function App() {
   return (
-    <>
+    <div className='flex flex-col min-h-screen relative'>
       <Navbar />
-      <main className='bg-white relative'>
+      <main className='relative flex-1'>
         <Dashboard />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
--- src/components/atoms/CartItem.tsx
+++ src/components/atoms/CartItem.tsx
@@ -1,26 +1,57 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-import {TbBasketCancel} from 'react-icons/tb';
+import { TbBasketCancel } from 'react-icons/tb';
 
-const CartItem=({item,updateQuantity,removeFromCart}: {item: any; updateQuantity: any; removeFromCart: any;}) => (
-    <li key={item.product.id} className="mb-4 text-gray-300">
-        <div className="flex justify-between items-center">
-            <div>
-                <p className="font-semibold">{item.product.title}</p>
-                <p>${item.product.price.toFixed(2)}</p>
-            </div>
-            <div className='flex flex-row items-center justify-center'>
+const CartItem = ({
+  item,
+  updateQuantity,
+  removeFromCart,
+}: {
+  item: any;
+  updateQuantity: any;
+  removeFromCart: any;
+}) => (
+  <li key={item.product.id} className='mb-4 text-gray-300'>
+    <div className='flex justify-between items-center'>
+      <div>
+        <p className='font-semibold'>{item.product.title}</p>
+        <p>${item.product.price.toFixed(2)}</p>
+      </div>
+      <div className='flex flex-row items-center justify-center'>
+        <div className='flex items-center'>
+          {/* Boton para restar cantidad */}
+          {item.quantity === 1 ? null : (
+            <button
+              className='px-2   text-red-500'
+              onClick={(): string =>
+                updateQuantity(item.product.title, item.quantity - 1)
+              }
+            >
+              -
+            </button>
+          )}
+          <span className='px-2'>{item.quantity}</span>
 
-                <div className="flex items-center">
-                    <button className="px-2   text-red-500" onClick={(): string => updateQuantity(item.product.title,item.quantity-1)}>-</button>
-                    <span className="px-2">{item.quantity}</span>
-                    <button className="px-2 text-green-500" onClick={(): string => updateQuantity(item.product.title,item.quantity+1)}>+</button>
-                </div>
-                <button className="px-2 text-red-500" onClick={(): string => removeFromCart(item.product.title)}>
-                    <TbBasketCancel className='h-5 w-5' />
-                </button>
-            </div>
+          {/* Boton para agregar cantidad */}
+          <button
+            className='px-2 text-green-500'
+            onClick={(): string =>
+              updateQuantity(item.product.title, item.quantity + 1)
+            }
+          >
+            +
+          </button>
         </div>
-    </li>
+        {/* Boton para eliminar del carrito */}
+        <button
+          className='px-2 text-red-500'
+          onClick={(): string => removeFromCart(item.product.title)}
+        >
+          <TbBasketCancel className='h-5 w-5' />
+        </button>
+      </div>
+    </div>
+  </li>
 );
 
 export default CartItem;
