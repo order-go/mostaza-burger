3f8038aec472260e74ef9c12bb9590e7cd38b53f
rauljariasz <rauljariasz@gmail.com>
2024-07-16 12:55:16 -0400

    FIX: images 🚀

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -40,8 +40,8 @@ const data: {
           title: 'Perro Normal',
           price: 0.99,
           images: [
-            '/Perros/2PerrosAsquerocitos.jfif',
-            '/Perros/2PerrosCalientes.jfif',
+            '/mostaza-burger/Perros/2PerrosAsquerocitos.jfif',
+            '/mostaza-burger/Perros/2PerrosCalientes.jfif',
             '/mostaza-burger/Perros/PerroCaliente.jfif',
           ],
           description:
@@ -51,7 +51,7 @@ const data: {
           id: 2,
           title: 'Perros Americanos',
           price: 1.49,
-          images: ['/Perros/combodog.jpg'],
+          images: ['/mostaza-burger/Perros/combodog.jpg'],
           description:
             'Perros americanos tradicionales salchica americana salsa blanca y salsa roja.',
         },
@@ -66,8 +66,8 @@ const data: {
           title: 'Hamburguesa Normal',
           price: 1.99,
           images: [
-            '/burger/RompeDietaBurger.jfif',
-            '/burger/RompeDietaBurger.jfif',
+            '/mostaza-burger/burger/RompeDietaBurger.jfif',
+            '/mostaza-burger/burger/RompeDietaBurger.jfif',
           ],
           description:
             'Hamburguesa clasica frescos ingredientes como: lechuga, tomate, cebolla, papas fritas, carne, salsas al gusto.',
@@ -76,7 +76,7 @@ const data: {
           id: 2,
           title: 'Burguer Comb',
           price: 4.99,
-          images: ['/burger/BurgerComb.jpg'],
+          images: ['/mostaza-burger/burger/BurgerComb.jpg'],
           description:
             'Doble hamburguesa con racion de papas cuenta con Lechuga tomate cebolla pepinillos y carne de res.',
         },
@@ -84,7 +84,7 @@ const data: {
           id: 3,
           title: 'Burger Papas',
           price: 1.99,
-          images: ['/burger/burgerpapas.jpg'],
+          images: ['/mostaza-burger/burger/burgerpapas.jpg'],
           description:
             'Extraña combinacion entre el morbido queso fundido y la carne acompañada de deliciosas papas fritas perfecta para cualquier antojo.',
         },
@@ -92,7 +92,7 @@ const data: {
           id: 4,
           title: 'Burger Express',
           price: 3.99,
-          images: ['/burger/rompehuesos.jpg'],
+          images: ['/mostaza-burger/burger/rompehuesos.jpg'],
           description:
             'Hamburguesa express cuenta con doble carne racion de papas tomate lechuga aros de cebolla y todo el contorno extra en queso y salsas',
         },
@@ -107,8 +107,8 @@ const data: {
           title: 'Pepito Carne',
           price: 7.99,
           images: [
-            '/pepito/PepitoFullEquipo.jfif',
-            '/pepito/PepitoPeligro.jfif',
+            '/mostaza-burger/pepito/PepitoFullEquipo.jfif',
+            '/mostaza-burger/pepito/PepitoPeligro.jfif',
           ],
           description:
             'Un tradicional pepito callejero de carne con: tomate, lechuga, papas cabello de angel, queso amarillo, cebolla, jamon, salsas al gusto.',
@@ -124,8 +124,8 @@ const data: {
           title: 'Salchipapas',
           price: 4.99,
           images: [
-            '/salchipapa/salchipapas.jpg',
-            '/salchipapa/salchipapas2.jpg',
+            '/mostaza-burger/salchipapa/salchipapas.jpg',
+            '/mostaza-burger/salchipapa/salchipapas2.jpg',
           ],
           description:
             'Combo de salchicas picadas papas fritas combinadas con salsas al gusto, queso amarillo, jamon y tocineta.',
@@ -140,7 +140,7 @@ const data: {
           id: 1,
           title: 'Combo 2 personas',
           price: 4.99,
-          images: ['/combos/combofamiliar.jpg'],
+          images: ['/mostaza-burger/combos/combofamiliar.jpg'],
           description:
             'Combo para dos personas dos perros calientes, 2 racion de pollo y papas fritas mas 2 jugos naturales.',
         },
@@ -148,7 +148,7 @@ const data: {
           id: 2,
           title: 'Papas fritas',
           price: 2.99,
-          images: ['/combos/papasfritas.jpg'],
+          images: ['/mostaza-burger/combos/papasfritas.jpg'],
           description:
             'Racion extensa de papas fritas con salsa tartara para abrir el apetito.',
         },
@@ -163,9 +163,9 @@ const data: {
           title: 'Coca Cola 1Lts',
           price: 0.99,
           images: [
-            '/bebidas/CocaCola.jfif',
-            '/bebidas/Coca-Cola2.jfif',
-            '/bebidas/Coca-Cola3.jfif',
+            '/mostaza-burger/bebidas/CocaCola.jfif',
+            '/mostaza-burger/bebidas/Coca-Cola2.jfif',
+            '/mostaza-burger/bebidas/Coca-Cola3.jfif',
           ],
           description:
             'Bebida Coca-Cola de 1Lt perfecta para acompañar tus pedidos en pareja.',
@@ -174,7 +174,10 @@ const data: {
           id: 2,
           title: 'Vaso Coca Cola',
           price: 0.5,
-          images: ['/bebidas/basoCoca.jpg', '/bebidas/Coca-Cola2.jfif'],
+          images: [
+            '/mostaza-burger/bebidas/basoCoca.jpg',
+            '/mostaza-burger/bebidas/Coca-Cola2.jfif',
+          ],
           description:
             'Bebida Coca-Cola  perfecta para acompañar tus pedidos individuales.',
         },
@@ -182,14 +185,14 @@ const data: {
           id: 3,
           title: 'Batido de cambur',
           price: 0.99,
-          images: ['/bebidas/batidocambur.jpg'],
+          images: ['/mostaza-burger/bebidas/batidocambur.jpg'],
           description: 'Delicioso batido de cambur muy nutritivo y natural.',
         },
         {
           id: 4,
           title: 'Batido de naranja',
           price: 0.99,
-          images: ['/bebidas/batidosabroso.jpg'],
+          images: ['/mostaza-burger/bebidas/batidosabroso.jpg'],
           description:
             'Jugo de naranja para los amantes del citrico, altos valores de vitamina C.',
         },
